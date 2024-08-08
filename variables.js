82bd6c330a0dc70c50e93b9902837f93d8283ae1
Melissa <mpa.alicando+github@gmail.com>
2024-08-08 21:35:06 +0800

    Adding in variables.js

---
+++ variables.js
@@ -0,0 +1,20 @@
+// Numbers
+
+let x = 5
+console.log(x)
+
+let y = 5.5
+console.log(y)
+
+// Strings
+
+let z = "Hello"
+console.log(z)
+
+let a = "String can be a full sentence"
+console.log(a)
+
+// Boolean
+
+let b = true
+console.log(b)
