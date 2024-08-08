1bbd4eca97c48625c6fd429f9d8ed2eec428f176
Melissa <mpa.alicando+github@gmail.com>
2024-08-08 21:54:05 +0800

    operations on variables

--- variables.js
+++ variables.js
@@ -18,3 +18,39 @@ console.log(a)
 
 let b = true
 console.log(b)
+
+// Reassigning variables
+
+let c = 3
+console.log(c)
+c = 4
+console.log(c)
+
+// Constant
+
+const d = 6
+console.log(d)
+// d = 7 - cannot reassign a constant
+
+// Operations on numbers
+
+let e = 3
+let f = 4
+
+let g = e + f
+console.log(g)
+
+let h = e - f
+console.log(f)
+
+let i = e * f
+console.log(i)
+
+let j = e / f
+console.log(j)
+
+let k = f % e
+console.log(k)
+
+e = e + 1
+console.log(e)
